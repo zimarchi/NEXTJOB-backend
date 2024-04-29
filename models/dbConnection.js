3554a5af26190dbfa66d6ba6f3cca332394d68bb
Nazim <nazim.messaoudene@pm.me>
2024-04-29 04:08:03 +0200

    premiere route backend vers bdd

---
+++ models/dbConnection.js
@@ -0,0 +1,20 @@
+const { Client } = require ('pg')
+
+const config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'apec',
+    user: 'postgres',
+    password: '011985',
+}
+
+const Apec = new Client (config)
+
+Apec.connect()
+    .then (() => console.log('database connected !!!'))
+    .catch (error => console.error(error))
+
+/*client.query('SELECT * FROM users')
+.then ((result) => console.log("result", result))*/
+
+module.exports = Apec;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const Apec = require('../models/dbConnection');
+
+// GET users listing
+router.get('/', async function (req, res, next) {
+  const result = await Apec.query('SELECT * FROM users')
+  res.json(result);
 });
+
+router.post('/', async function (req, res, next) {
+  let {firstname, lastname, email, pwd, has_profile, gender} = req.body
+  await Apec.query (
+    `INSERT INTO users 
+      (firstname, lastname, email, pwd, has_profile, gender, creation_date)
+      VALUES (
+        ${firstname},
+        ${lastname}, 
+        ${email}, 
+        ${pwd}, 
+        ${has_profile}, 
+        ${gender}, 
+        current_timestamp)`
+  )
+  const result = await Apec.query('SELECT * FROM users')
+  res.json (result)  
+});
+
+
+
+
 
 module.exports = router;
