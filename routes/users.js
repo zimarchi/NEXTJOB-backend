7561664c299d2d4c9aa188b9c2d5b5a3f0ef01aa
Nazim <nazim.messaoudene@pm.me>
2024-05-22 04:10:08 +0200

    signup route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const Apec = require('../models/dbConnection');
+const bcrypt = require('bcrypt');
 
 // GET users listing
 router.get('/', async function (req, res, next) {
@@ -9,23 +10,35 @@ router.get('/', async function (req, res, next) {
   res.json(result);
 });
 
-router.post('/', async function (req, res, next) {
-  let {firstname, lastname, email, pwd, has_profile, gender} = req.body
-  await Apec.query (
-    `INSERT INTO users 
-      (firstname, lastname, email, pwd, has_profile, gender, creation_date)
-      VALUES (
-        ${firstname},
-        ${lastname}, 
-        ${email}, 
-        ${pwd}, 
-        ${has_profile}, 
-        ${gender}, 
-        current_timestamp)`
-  )
-  const result = await Apec.query('SELECT * FROM users')
-  res.json (result)  
+//SIGN UP
+router.post('/signup', async (req, res) => { 
+  const { firstname, lastname, email, pwd, gender } = req.body; 
+
+  try { 
+    const utilisateurExiste = await Apec.query( 'SELECT * FROM users WHERE email = $1', [email] );
+    if (utilisateurExiste.rows.length > 0) { // Vérifier si l'utilisateur existe déjà
+      return res.status(400).send('Cet utilisateur existe déjà.')
+    } 
+    // Hasher le mot de passe
+    const pwdHash = bcrypt.hashSync(pwd, 10);
+    
+    // Créer l'utilisateur dans la base de données
+    const resultat = await Apec.query ('INSERT INTO users (firstname, lastname, email, pwd, has_profile, gender, creation_date) VALUES ($1, $2, $3, $4, false, $5, current_timestamp)', [firstname, lastname, email, pwdHash, gender])
+    
+    //res.status(201).send('Compte créé avec succès.');
+    const result = await Apec.query('SELECT * FROM users')
+    res.json (result)
+    }
+
+  catch (err) {
+    console.error(err);
+    res.status(500).send('Erreur serveur.');
+  }
+  
+
 });
+
+
 
 
 
