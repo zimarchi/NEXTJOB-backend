6bc4fb6145752d4291b186db06af8a1519256bfe
Nazim MESSAOUDENE <nazimmessaoudene@Lis-MAC-23-01.local>
2025-02-19 15:52:20 +0100

    reprise de la bdd

---
+++ routes/candidats.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+const Apec = require('../models/dbConnection');
+const bcrypt = require('bcrypt');
+
+// GET liste des candidats
+router.get('/', async function (req, res, next) {
+  const result = await Apec.query('SELECT * FROM candidats')
+  res.json(result);
+});
+
+//SIGN UP
+router.post('/signup', async (req, res) => { 
+  const { firstname, lastname, email, pwd } = req.body; 
+
+  try { 
+    const candidatExiste = await Apec.query( 'SELECT * FROM candidats WHERE email = $1', [email] );
+    if (candidatExiste.rows.length > 0) { // Vérifier si le candidat existe déjà
+      return res.status(400).send('Cet utilisateur existe déjà.')
+    } 
+    // Hasher le mot de passe
+    const pwdHash = bcrypt.hashSync(pwd, 5);
+    
+    // Créer le candidat dans la base de données
+    const creation = await Apec.query ('INSERT INTO candidats (firstname, lastname, email, pwd, creation_date) VALUES ($1, $2, $3, $4, current_timestamp)', [firstname, lastname, email, pwdHash])
+    
+    //Vérification du résultat : 
+    // méthode 1 : res.status(201).send('Compte créé avec succès.');
+    // méthode 2 :
+    const result = await Apec.query('SELECT * FROM candidats')
+    res.json (result)
+    }
+
+  catch (err) {
+    console.error(err);
+    res.status(500).send('Erreur serveur.');
+  }
+
+});
+
+
+
+
+
+
+
+module.exports = router;
